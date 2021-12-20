643719c73750d3b1e89fc84c5a9ee0410f4f020d
killerkc12 <kiranchavankc12@gmail.com>
2021-12-20 13:17:21 +0530

    Login added

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,8 +11,11 @@ import {
     DotsCircleHorizontalIcon,
     DotsHorizontalIcon,
     } from "@heroicons/react/outline";
+import { signOut, useSession } from 'next-auth/react';
 
 function Sidebar() {
+    const {data: session} = useSession();
+
     return (
         <div className="hidden 
             sm:flex flex-col items-center 
@@ -40,12 +43,14 @@ function Sidebar() {
             font-bold shadow-md hover:bg-[#1a8cd8]">Tweet</button>
             <div className="text-[#d9d9d9] flex items-center justify-center
             hoverAnimation 
-            xl:ml-auto xl:mr-5 mt-auto">
-                <img src="https://rb.gy/ogau5a" alt="img" 
+            xl:ml-auto xl:mr-5 mt-auto"
+            onClick={signOut}
+            >
+                <img src={session.user.image} alt="img" 
                 className="h-10 w-10 rounded-full xl:mr-2.5"/>
                 <div className="hidden xl:inline leading-5">
-                    <h4 className="font-bold">Kiran Chavan</h4>
-                    <p className="text-[#6e767d]">@kiran_n_chavan</p>
+                    <h4 className="font-bold">{session.user.name}</h4>
+                    <p className="text-[#6e767d]">@{session.user.tag}</p>
                 </div>
                 <DotsHorizontalIcon className="h-5 hidden xl:inline ml-10" />
             </div>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
+import { getProviders, getSession } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
+import Login from '../components/Login'
 
-export default function Home() {
+export default function Home({trendingResults, followResults, providers}) {
+
+  const {data: session} = useSession();
+
+  if (!session) return <Login providers={providers}/>
+
   return (
     <div className="">
       <Head>
@@ -18,6 +26,7 @@ export default function Home() {
 
         {/* Feed  */}
         <Feed/>
+        {/* {session.user.name} */}
         
         {/* Widgets  */}
 
@@ -25,4 +34,24 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
 }
