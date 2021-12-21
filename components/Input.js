bf8ff3501018bd4a5d2b1c3263b8d7232f97a459
killerkc12 <kiranchavankc12@gmail.com>
2021-12-22 01:30:02 +0530

    widget done

--- components/Input.js
+++ components/Input.js
@@ -66,7 +66,7 @@ function Input() {
 
     return (
         <div className={`border-b border-gray-700 p-3 flex
-            space-x-3 overflow-y-scroll
+            space-x-3 
             ${loading && "opacity-60"}
             `}>
             <img src={session.user.image} alt="icon" 
